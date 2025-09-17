ac251e7ce765bcb3bb67d30751f5958b888e25a7
Nataliia Tur <natalim@ukr.net>
2025-09-17 15:45:03 +0300

    added lazy and suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage.jsx";
-import CatalogPage from "./pages/CatalogPage/CatalogPage.jsx";
-import CamperDetailsPage from "./pages/CamperDetailsPage/CamperDetailsPage.jsx";
-import FavoritesPage from "./pages/FavoritesPage/FavoritesPage.jsx";
+import { lazy, Suspense } from "react";
 import Layout from "./components/common/Layout/Layout.jsx";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage.jsx"));
+const CamperDetailsPage = lazy(() =>
+  import("./pages/CamperDetailsPage/CamperDetailsPage.jsx")
+);
+const FavoritesPage = lazy(() =>
+  import("./pages/FavoritesPage/FavoritesPage.jsx")
+);
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:id" element={<CamperDetailsPage />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
-          <Route path="404" element={<NotFoundPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/catalog/:id" element={<CamperDetailsPage />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+            <Route path="404" element={<NotFoundPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
